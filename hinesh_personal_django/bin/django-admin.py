15140a6fc29bad9b8a87d2cb2c0a2700021539c9
Hinesh Patel <hineshpatel0@googlemail.com>
2017-06-14 20:44:31 +0100

    Initial commit

---
+++ hinesh_personal_django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hineshp/Documents/programming/Python/Django/hinesh_personal_django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
